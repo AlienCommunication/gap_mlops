5217c1243c5aebbf59329aa499cec92f1466ae28
Amit <amit.yadav1.contractor@pepsico.com>
2025-01-04 15:21:47 +0530

    Updated BigQuery dataset and table names

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 PROJECT_ID = "classdemo-425210"
 BUCKET_NAME = "gs://vertex-mlops-bucket990"
 BQ_DATASET = "bigquery-public-data.ml_datasets"
-BQ_TABLE = "california_housing"
+BQ_TABLE = "california_housing_prices"  # Corrected table name
 MODEL_NAME = "california_housing_model"
 REGION = "us-central1"
