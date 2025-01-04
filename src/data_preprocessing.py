cb344ae319e573fa3704ab60918c9b5a07fdfe5a
Amit <amit.yadav1.contractor@pepsico.com>
2025-01-04 14:57:35 +0530

    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,5 +1,7 @@
 from google.cloud import bigquery
 import pandas as pd
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from google.cloud import storage
 from config import settings as cfg
 
