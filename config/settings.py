89766c7160b9b48591b0c52f8c432ec322a3cfe5
Amit <amit.yadav1.contractor@pepsico.com>
2025-01-04 16:42:47 +0530

    I/O

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 PROJECT_ID = "classdemo-425210"
-BUCKET_NAME = "gs://vertex-mlops-bucket990"
+BUCKET_NAME = "vertex-mlops-bucket990"
 BQ_DATASET = "bigquery-public-data.new_york_taxi_trips"
 BQ_TABLE = "tlc_yellow_trips_2022"
 MODEL_NAME = "nyc_taxi_model"
